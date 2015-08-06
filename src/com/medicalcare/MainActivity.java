dc49d23b507b867ea85ee84d3aa479f20745c802
liuhexiang <670527152@qq.com>
2015-08-06 15:24:29 +0800

    毛病快出来吧

--- src/com/medicalcare/MainActivity.java
+++ src/com/medicalcare/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends Activity implements LocationSource,AMapLocatio
 	private Button chooes2;
 	private ViewFlipper flipper; */
 	
-	//adfasdfasdfdsfa宿舍
+	//adfasdfasdfdsfa宿舍ff
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
